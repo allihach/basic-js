0c2b395df94d35ef38a37933909ce1d9bc7d4be1
allihach <lihach.ac@gmail.com>
2020-03-22 03:04:13 +0400

    feat: completed task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,78 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(bool) {
+        this.bool = bool;
+    }
+    
+    encrypt(message, key) {
+       if(message == undefined || key == undefined) {
+           throw Error;
+       }
+       let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       let result = '';
+       message = message.toUpperCase();
+       key = key.toUpperCase();
+       let indexA;
+    
+       for(let m=0, k=0; m<message.length; m++, k++) {
+            if(k>key.length-1) {
+                k = 0;
+            }
+
+           if(a.indexOf(message[m]) == -1) {
+               result += message[m];
+               k--;
+               continue;
+           }
+           if(a.indexOf(key[k])+a.indexOf(message[m])>25) {
+              indexA = a.indexOf(key[k])+a.indexOf(message[m])-26;
+           } else {
+              indexA = a.indexOf(key[k])+a.indexOf(message[m]);
+           }
+
+           result += a[indexA];
+       }
+       return result;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        
+        if(message == undefined || key == undefined) {
+            throw Error;
+        }
+
+        let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let result = '';
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let indexA;
+
+        if(this.bool == false) {
+            message = message.split('').reverse().join('');
+        }
+
+        for(let m=0, k=0; m<message.length; m++, k++) {
+            if(k>key.length-1) {
+                k = 0;
+            }
+
+            if(a.indexOf(message[m]) == -1) {
+                result += message[m];
+                k--;
+                continue;
+            }
+
+            if(a.indexOf(message[m])-a.indexOf(key[k])<0) {
+                indexA = 26-Math.abs(a.indexOf(message[m])-a.indexOf(key[k]));
+            } else {
+                indexA = a.indexOf(message[m])-a.indexOf(key[k]);
+            }
+            
+            result += a[indexA];
+        }
+        if(this.bool==false) {
+            return result.split('').reverse().join('');
+        }
+        return result;
     }
 }
 
